2d109bf87f7968f851ede1f1404234a3f409aa30
Kelvin Clement Mwinuka <KelvinMwinuka@hotmail.co.uk>
2022-04-11 01:21:37 +0300

    Retreive code from 'event.request.codeParameter' in custom_message trigger to prevent email message error.

--- src/custom_message.js
+++ src/custom_message.js
@@ -1,41 +1,27 @@
 'use strict';
-require("dotenv").config({})
+require("dotenv").config({});
 
 module.exports.handler = async (event, context, callback) => {
   switch(event.triggerSource) {
     case "CustomMessage_SignUp":
-      event.response = {
-        ...event.response,
-        smsMessage: `Hi ${event.usernameParameter}, your signup code is ${event.codeParameter}.`,
-        emailSubject: `Your registration code.`,
-        emailMessage: `Hi ${event.usernameParameter}, your registration code is ${event.codeParameter}.`
-      }
-      break;
-    case "CustomMessage_ResendCode":
-      event.response = {
-        ...event.response,
-        smsMessage: `Hi ${event.usernameParameter}, your requested code is ${event.codeParameter}`,
-        emailSubject: `Your requested code.`,
-        emailMessage: `Hi ${event.usernameParameter}, your requested verification code is ${event.codeParameter}`
-      }
+      event.response.smsMessage = `Hi ${event.userName}, your signup code is ${event.request.codeParameter}`;
+      event.response.emailSubject = `Your registration code`;
+      event.response.emailMessage = `Hi ${event.userName}, your signup code is ${event.request.codeParameter}`;
       break;
     case "CustomMessage_ForgotPassword":
-      event.response = {
-        ...event.response,
-        smsMessage: `Hi ${event.usernameParameter}, your password reset code is ${event.codeParameter}. 
-        If you did not request this code, ignore this message. Please DO NOT share this code with anyone.`,
-        emailSubject: `Your password reset code.`,
-        emailMessage: `Hi ${event.usernameParameter}, your password reset code is ${event.codeParameter}. 
-        If you did not request this code, ignore this email. Please DO NOT share this code with anyone.`
-      }
+      event.response.smsMessage = `Hi ${event.userName}, your password reset code is ${event.request.codeParameter}. If you did not request this code, ignore this message. Please DO NOT share this code with anyone.`;
+      event.response.emailSubject = `Your password reset code`;
+      event.response.emailMessage = `Hi ${event.userName}, your password reset code is ${event.request.codeParameter}. If you did not request this code, ignore this email. Please DO NOT share this code with anyone.`;
+      break;
+    case "CustomMessage_ResendCode":
+      event.response.smsMessage = `Hi ${event.userName}, your requested code is ${event.request.codeParameter}`;
+      event.response.emailSubject = `Your requested code`;
+      event.response.emailMessage = `Hi ${event.userName}, your requested verification code is ${event.request.codeParameter}`;
       break;
     default:
-      event.response = {
-        ...event.response,
-        smsMessage: `Hi, ${event.usernameParameter}, your code is ${event.codeParameter}.`,
-        emailSubject: `Your code`,
-        emailMessage: `Hi, ${event.usernameParameter}, your code is ${event.codeParameter}`
-      }
+      event.response.smsMessage = `Hi ${event.userName}, your requested code is ${event.request.codeParameter}`;
+      event.response.emailSubject = `Your requested code`;
+      event.response.emailMessage = `Hi ${event.userName}, your requested code is ${event.request.codeParameter}`;
   }
-  callback(null, event)
+  callback(null, event);
 }
