fde55bde1fc03c9ed50ddadfd35bfd34d591eedd
Kelvin Clement Mwinuka <KelvinMwinuka@hotmail.co.uk>
2022-04-10 21:43:55 +0300

    Implemented custom response logic

--- src/custom_message.js
+++ src/custom_message.js
@@ -2,5 +2,40 @@
 require("dotenv").config({})
 
 module.exports.handler = async (event, context, callback) => {
+  switch(event.triggerSource) {
+    case "CustomMessage_SignUp":
+      event.response = {
+        ...event.response,
+        smsMessage: `Hi ${event.usernameParameter}, your signup code is ${event.codeParameter}.`,
+        emailSubject: `Your registration code.`,
+        emailMessage: `Hi ${event.usernameParameter}, your registration code is ${event.codeParameter}.`
+      }
+      break;
+    case "CustomMessage_ResendCode":
+      event.response = {
+        ...event.response,
+        smsMessage: `Hi ${event.usernameParameter}, your requested code is ${event.codeParameter}`,
+        emailSubject: `Your requested code.`,
+        emailMessage: `Hi ${event.usernameParameter}, your requested verification code is ${event.codeParameter}`
+      }
+      break;
+    case "CustomMessage_ForgotPassword":
+      event.response = {
+        ...event.response,
+        smsMessage: `Hi ${event.usernameParameter}, your password reset code is ${event.codeParameter}. 
+        If you did not request this code, ignore this message. Please DO NOT share this code with anyone.`,
+        emailSubject: `Your password reset code.`,
+        emailMessage: `Hi ${event.usernameParameter}, your password reset code is ${event.codeParameter}. 
+        If you did not request this code, ignore this email. Please DO NOT share this code with anyone.`
+      }
+      break;
+    default:
+      event.response = {
+        ...event.response,
+        smsMessage: `Hi, ${event.usernameParameter}, your code is ${event.codeParameter}.`,
+        emailSubject: `Your code`,
+        emailMessage: `Hi, ${event.usernameParameter}, your code is ${event.codeParameter}`
+      }
+  }
   callback(null, event)
 }
